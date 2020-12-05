9812b063a181736a22d90f2921435e48787f40c2
Bruno Rafael Filgueira Dias <brunor_17@hotmail.com>
2020-12-04 21:47:49 -0300

    implementando a validação de tipo do arquivo

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,5 +1,20 @@
 const fs = require('fs');
+const path = require('path');
 
-fs.createReadStream('./assets/img/autumn-forest.jpg')
-  .pipe(fs.createWriteStream('./assets/img/autumn-forest-2.jpg'))
-  .on('finish', () => console.log('imagem escrita com sucesso'));
+module.exports = (caminho, nomeArquivo, callbackImagemCriada) => {
+  const tiposValidos = ['jpg', 'png', 'jpeg'];
+  const tipo = path.extname(caminho);
+  const tipoEValido = tiposValidos.indexOf(tipo.substring(1)) !== -1;
+
+  if (!tipoEValido) {
+    const erro = 'Tipo inválido';
+    callbackImagemCriada(erro);
+    console.log('Erro. Tipo inválido');
+  } else {
+    const novoCaminho = `./assets/img/${nomeArquivo}${tipo}`;
+
+    fs.createReadStream(caminho)
+      .pipe(fs.createWriteStream(novoCaminho))
+      .on('finish', () => callbackImagemCriada(false, novoCaminho));
+  }
+};
